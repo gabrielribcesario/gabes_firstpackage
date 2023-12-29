17a71dd64da5585116b4208f966fb012bc2b522c
Mastercookies <gabrielc.ribeiro@hotmail.com>
2023-12-29 19:06:20 -0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,16 +1,15 @@
-import numpy as np
 from copy import deepcopy
-
-#https://stackoverflow.com/a/23093013
-
 from os import listdir
 from os.path import abspath, dirname, isfile, join
+import numpy as np
 
 # get location of __init__.py and get folder name of __init__.py
-init_dir = dirname(abspath(__file__))
-# get all python files
-py_files = [file_name.replace(".py", "") for file_name in listdir(init_dir) if isfile(join(init_dir, file_name)) and ".py" in file_name and not ".pyc" in file_name]
-# remove this __init__ file from the list
-py_files.remove("__init__")
+init_dir = listdir(dirname(abspath(__file__)))
+init_dir.remove("__init__.py")
 
-__all__ = py_files
+modulename = ['deepcopy', 'listdir', 'abspath', 'dirname', 'isfile', 'join', 'np']
+for file_name in init_dir:
+  if isfile(join(init_dir, file_name)) and ".py" in file_name and not ".pyc" in file_name:
+    modulename.append(filename.replace('.py', ''))
+    
+__all__ = modulename
