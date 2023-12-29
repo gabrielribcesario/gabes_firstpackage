217722d09824ac618c35491df4bff5dc998a4c7a
Mastercookies <gabrielc.ribeiro@hotmail.com>
2023-12-29 15:37:57 -0300

    Create Func.py

---
+++ Func.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class Func():
+  
+  def sigmoid(X):
+    return (1/(1+np.exp(-X)))
+  
+  def der_sigmoid(X):    
+    def sigmoid(X):
+      return (1/(1+np.exp(-X)))
+    sigma = sigmoid(X)
+    return (sigma*(1 - sigma))
+  
+  def der_tanh(X):
+    return np.cosh(X)**-2
+  
+  def ReLU(X):
+    return (X + abs(X))/2
+  
+  def der_ReLU(X):
+    return (X > 0) * 1
+  
+  def sinc(X):
+    return np.sin(X)/X
+  
+  def der_sinc(X):
+    return (X*np.cos(X) - np.sin(X))/X**2
+  
+  def x_squared(X):
+    return X**2
+  
+  def der_x_squared(X):
+    return 2*X
+  
+  def lse(x, y):
+    return (x - y)**2
+  
+  def acc(x, y):
+    return ( (x >= .5)*1 != y)*1
+
